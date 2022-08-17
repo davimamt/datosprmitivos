fa663385f87257bf9b5f8035c8a5fa541b0842b5
fapcod <leyenda1997@gmail.com>
2022-08-17 17:37:26 -0500

    se crean datos primitivos

---
+++ main.py
@@ -0,0 +1,15 @@
+# Hola me llamo comentario de linea
+
+'''
+Hola soy un comentario de bloque
+'''
+
+#Datos primitivos
+nombre="David"
+edad=29
+estatura=1.88
+hinchadelmejor=False
+
+#Salidas
+print("Hola: ",nombre, "tu estatura", estatura)
+print(f'hola {nombre} tu estatura es {estatura} y tu edad es {edad}')
